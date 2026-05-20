35ceebdcefe8a429478d7fad480a93ff3531645e
David Ziliak <10733311+dziliak@users.noreply.github.com>
2026-05-20 16:05:39 -0500

    removed hypr+w command for hello world in hammerspoon

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -2,10 +2,6 @@ hs.ipc.cliInstall()
 
 local hyper = { "cmd", "alt", "ctrl" }
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-	hs.notify.new({ title = "Hammerspoon", informativeText = "Hello World" }):send()
-end)
-
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "E", function()
 	hs.alert.show("Hello World!")
 end)
